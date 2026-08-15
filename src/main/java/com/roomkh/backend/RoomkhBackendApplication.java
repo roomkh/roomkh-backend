4f7d7ad9b5a969a781c9e029f448db734dd108b4
Suy Piseth <suyp8944@gmail.com>
2026-08-15 19:44:43 +0700

    chore: initialize Spring Boot PostgreSQL Docker

---
+++ src/main/java/com/roomkh/backend/RoomkhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roomkh.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomkhBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomkhBackendApplication.class, args);
+    }
+
+}
